5219b8397ff76f648ccf5828948e263fbd43153b
Zaniar Shokati <zaniar.shokati@gmail.com>
2023-03-20 13:36:56 +0100

    remove comments

--- Layers/SoftMax.py
+++ Layers/SoftMax.py
@@ -7,12 +7,10 @@ class SoftMax:
 
     def forward(self, input_tensor):
         max_row = np.max(input_tensor, axis=1, keepdims=True)
-        # max_row = max_row.reshape(np.size(input_tensor, axis=0), 1)
         input_tensor -= max_row
 
         ex_x = np.exp(input_tensor)
         sum_exp_x = np.sum(ex_x, axis=1, keepdims=True)
-        #sum_exp_x = sum_exp_x.reshape(np.size(input_tensor, axis=0), 1)
         self.y_hat = ex_x / sum_exp_x
 
         return self.y_hat
